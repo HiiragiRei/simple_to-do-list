b6ce3e61787d68fdbef904e62deb6d8aa79f9163
HiiragiRei <87363664+HiiragiRei@users.noreply.github.com>
2021-11-20 23:55:35 +0800

    all finished BUT have BUG
    all finished according to the tutorial but has several bug, need to fix
    And wait for improvement

--- app.js
+++ app.js
@@ -10,3 +10,42 @@ for(i=0; i<nodeList.length; ++i){
     nodeList[i].appendChild(span);
 }
 
+// click close button to hide current list item
+var close = document.getElementsByClassName("close");
+for(i=0; i<close.length; ++i){
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+// add "checked" symbol when clicking list item
+
+
+// create new list when clicking on "Add" button
+function newElement(){  // back at html <span onclick="newElement()" ...>
+    var li = document.createElement("li"); // = create new list
+    var inputValue = document.getElementById("myInput");
+    var node = document.createTextNode(inputValue);
+
+    li.appendChild(node);
+    if(inputValue='') // if user didn't type anything
+        alert("You must write something!");
+    else
+        document.getElementById("myList").appendChild(li);
+
+    document.getElementById("myInput").value="";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className="close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for(i=0; i<close.length; ++i){
+        close[i].onclick = function(){
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
